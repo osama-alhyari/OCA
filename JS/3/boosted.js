5cdd49fbcefa7e9bfb566b583f8cad1c1b296029
Osama <osamaalhyari99@gmail.com>
2024-06-11 12:30:10 +0300

    session updates

--- JS/3/boosted.js
+++ JS/3/boosted.js
@@ -1,19 +1,38 @@
+let idCount;
+if (localStorage.getItem(idCount)) {
+  idCount = localStorage.getItem(idCount);
+} else {
+  idCount = 1;
+}
+
+let users;
+if (localStorage.getItem(users)) {
+  users = localStorage.getItem(users);
+} else {
+  users = [];
+}
+
 function checkDone() {
   let signUpButton = document.querySelector("#signup");
   let emailInput = document.querySelector("#email");
   let phoneInput = document.querySelector("#mobile");
   let passwordInput = document.querySelector("#password");
+  let nameInput = document.querySelector("#name");
   let checkBox = document.querySelector("#agree");
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const passwordPattern =
+  let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let passwordPattern =
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  const phonePattern = /^077\d{7}$/;
+  let phonePattern = /^077\d{7}$/;
+  let namePattern = /^[A-Za-z]+ [A-Za-z]+$/;
+  let confirmInput = document.querySelector("#confirm");
 
   if (
     phonePattern.test(String(phoneInput.value)) &&
     emailPattern.test(emailInput.value) &&
     passwordPattern.test(passwordInput.value) &&
-    checkBox.checked
+    namePattern.test(nameInput.value) &&
+    checkBox.checked &&
+    confirmInput.value === passwordInput.value
   ) {
     signUpButton.removeAttribute("disabled");
   } else {
@@ -21,13 +40,24 @@ function checkDone() {
   }
 }
 
+function nameCorrect() {
+  let errorMessage = document.querySelector("#errName");
+  let namePattern = /^[A-Za-z]+ [A-Za-z]+$/;
+  let nameInput = document.querySelector("#name");
+  if (namePattern.test(nameInput.value)) {
+    errorMessage.textContent = "";
+  } else {
+    errorMessage.textContent = "Please enter your name";
+  }
+}
+
 function passwordCorrect() {
   let passwordInput = document.querySelector("#password");
   let errorMessage = document.querySelector("#errPass");
-  const capitalLetterPattern = /[A-Z]/;
-  const lowerLetterPattern = /[a-z]/;
-  const numberPattern = /\d/;
-  const symbolPattern = /[!@#$%^&*(),.?":{}|<>]/;
+  let capitalLetterPattern = /[A-Z]/;
+  let lowerLetterPattern = /[a-z]/;
+  let numberPattern = /\d/;
+  let symbolPattern = /[!@#$%^&*(),.?":{}|<>]/;
   if (passwordInput.value.length < 8) {
     errorMessage.textContent = "Password must be 8 characters long";
   } else if (!capitalLetterPattern.test(passwordInput.value)) {
@@ -57,7 +87,7 @@ function emailCorrect() {
 function phoneCorrect() {
   let phoneInput = document.querySelector("#mobile");
   let errorMessage = document.querySelector("#errPhone");
-  let phoneNumberPattern = /^077\d{6}$/;
+  let phoneNumberPattern = /^077\d{7}$/;
   if (phoneNumberPattern.test(String(phoneInput.value))) {
     errorMessage.textContent = "";
   } else {
@@ -65,11 +95,29 @@ function phoneCorrect() {
   }
 }
 
+function confirmCorrect() {
+  let confirmInput = document.querySelector("#confirm");
+  let errorMessage = document.querySelector("#errConfirm");
+  if (confirmInput.value === document.querySelector("#password").value) {
+    errorMessage.textContent = "";
+  } else {
+    errorMessage.textContent = "Passwords must match";
+  }
+}
+
 function done() {
   let email = document.querySelector("#email").value;
   let number = document.querySelector("#mobile").value;
   let password = document.querySelector("#password").value;
-  localStorage.setItem("email", email);
-  localStorage.setItem("number", number);
-  localStorage.setItem("password", password);
+  let name = document.querySelector("#name").value;
+  let id = idCount;
+  idCount += 1;
+  let newUser = { id, name, email, number, password };
+  users.push(newUser);
+  localStorage.setItem("users", JSON.stringify(users));
+  localStorage.setItem("idCount", idCount);
+}
+
+function myLocation() {
+  window.location = "http://127.0.0.1:5500/login.html";
 }
--- JS/3/login.js
+++ JS/3/login.js
@@ -1,10 +1,23 @@
-function welcomeAlert() {
-  let credEmail = document.querySelector("#email").value;
-  let credPassword = document.querySelector("#password").value;
-  let email = localStorage.getItem("email");
-  let password = localStorage.getItem("password");
-  if (email === credEmail && password === credPassword) {
-    alert("you have logged in");
+function checkCreds(e) {
+  e.preventDefault();
+  let users = localStorage.getItem("users");
+  console.log(users);
+  users = JSON.parse(users);
+  console.log(users);
+  email = document.querySelector("#email").value;
+  password = document.querySelector("#password").value;
+  let found = false;
+  for (let i = 0; i < users.length; i += 1) {
+    if (email === users[i].email && password === users[i].password) {
+      sessionStorage.setItem("id", `${users[i].id}`);
+      sessionStorage.setItem("name", `${users[i].name}`);
+      found = true;
+      console.log(users[i]);
+      break;
+    }
+  }
+  if (found) {
+    alert(`logged in as ${sessionStorage.getItem("name")}`);
   } else {
     alert("wrong email or password");
   }
